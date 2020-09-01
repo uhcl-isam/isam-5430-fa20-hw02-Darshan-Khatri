41f4c6830cab02ca7956989177e6648eaebf727e
Darshan-Khatri <darshankhatri2297@gmail.com>
2020-08-31 19:50:24 -0500

    Conditional Statements :  CigarParty

--- CSharp.Assignments.Selection1/Logic1.cs
+++ CSharp.Assignments.Selection1/Logic1.cs
@@ -14,6 +14,20 @@ namespace CSharp.Assignments.Loop1
         /// <returns></returns>
         public static bool CigarParty(int cigars, bool isWeekend)
         {
+            if (isWeekend && cigars >= 40)
+            {
+                return true;
+            }
+            else if (cigars >= 40 && cigars <= 60)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
             throw new NotImplementedException();
         }
 
